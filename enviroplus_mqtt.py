aae733d299396c49f3872027911e47ea8158ef7e
cpethica <charles@pethica.net>
2022-02-17 11:13:05 +0000

    added new sensors, gas and particulates

--- enviroplus_mqtt.py
+++ enviroplus_mqtt.py
@@ -2,12 +2,17 @@
 
 import time
 from pms5003 import PMS5003, ReadTimeoutError
+from enviroplus import gas
+from bme280 import BME280
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 
 import paho.mqtt.client as mqtt
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.ERROR) 
 
-#Load configuration from .ini file.
+#Load configuration from .ini file
 import configparser
 import sys
 # make sure config file exists
@@ -27,10 +32,26 @@ MQTT_TOPIC = 'home/+/+'
 MQTT_REGEX = 'home/([^/]+)/([^/]+)'
 MQTT_CLIENT_ID = 'enviroplus'
 
+def on_disconnect(mqtt_client, userdata, rc):
+    logging.info("disconnecting reason  "  +str(rc))
+    mqtt_client.connected_flag=False
+    mqtt_client.disconnect_flag=True
+
+def on_connect(mqtt_client, userdata, flags, rc):
+    if rc==0:
+        mqtt_client.connected_flag=True
+    else:
+        print('bad connection')
+
 mqtt_client = mqtt.Client(MQTT_CLIENT_ID)
 mqtt_client.username_pw_set(MQTT_USER, MQTT_PASSWORD)
+mqtt_client.on_connect= on_connect
+mqtt_client.on_disconnect = on_disconnect
 mqtt_client.connect(MQTT_ADDRESS, 1883)
+mqtt_client.loop_start()
 
+
+bme280 = BME280()
 pms5003 = PMS5003()
 time.sleep(1.0)
 
@@ -40,17 +61,34 @@ time.sleep(1.0)
 # pm_ug_per_m3(2.5)
 # pm_ug_per_m3(10))
 
-
 while True:
+
+    if not mqtt_client.connected_flag:
+        try:
+            mqtt_client.connect(MQTT_ADDRESS, 1883)
+        except:
+            print('Disconnected')
+
     try:
         # wake sensor from low power mode and wait to stabilise
         GPIO.output(22, 1)
         time.sleep(30)
-        readings = pms5003.read()
-        print(f"PM per l air 0.1: {readings.pm_per_1l_air}")
+        pms_readings = pms5003.read()
+        gas_readings = gas.read_all()
+        pres = bme280.get_pressure()
+        temp = bme280.get_temperature()
+        hum = bme280.get_humidity()
 
-        mqtt_client.publish("home/indoor/PM10", readings.pm_ug_per_m3(10))
+        mqtt_client.publish("home/indoor/PM1", pms_readings.pm_ug_per_m3(1.0))
+        mqtt_client.publish("home/indoor/PM2.5", pms_readings.pm_ug_per_m3(2.5))
+        mqtt_client.publish("home/indoor/PM10", pms_readings.pm_ug_per_m3(10))
+        mqtt_client.publish("home/indoor/pressure", pres)
+        mqtt_client.publish("home/indoor/temperature", temp)
+        mqtt_client.publish("home/indoor/humidity", hum)
 
+        mqtt_client.publish("home/indoor/gas_reducing", gas_readings.reducing)
+        mqtt_client.publish("home/indoor/gas_oxidising", gas_readings.oxidising)
+        mqtt_client.publish("home/indoor/gas_nh3", gas_readings.nh3)
 
     except ReadTimeoutError:
         pms5003 = PMS5003()
